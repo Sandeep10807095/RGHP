7e48c300f147133131029a23c129c07e779f8c68
SandeepSrinivasDwaram <sandeepsrinivasdwaram@github.com>
2019-06-26 17:12:59 +0200

    Updates

---
+++ precache-manifest.883270478bbea34c3eea6ce62c79d224.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28be149097c387473d5f099f5c6c603c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93324c2efe5d06463e9d",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9f2186fb6044641b6fd5",
+    "url": "/static/js/2.5ae3991e.chunk.js"
+  },
+  {
+    "revision": "93324c2efe5d06463e9d",
+    "url": "/static/js/main.ec2fe26b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
